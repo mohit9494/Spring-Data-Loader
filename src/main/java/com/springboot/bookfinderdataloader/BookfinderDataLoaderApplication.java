a5401c2124ba0ffbb7a4fb66d6dd3b7a30c23464
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-03-18 13:03:51 -0400

    Adding Data Loader Initial Version

---
+++ src/main/java/com/springboot/bookfinderdataloader/BookfinderDataLoaderApplication.java
@@ -0,0 +1,28 @@
+package com.springboot.bookfinderdataloader;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BookfinderDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfinderDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+}
---
+++ src/test/java/com/springboot/bookfinderdataloader/BookfinderDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bookfinderdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfinderDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
